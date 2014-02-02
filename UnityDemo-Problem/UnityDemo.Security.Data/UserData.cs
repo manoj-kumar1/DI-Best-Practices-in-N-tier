7995e7b0471c745bf9c7fcef31310ddbd6f896a5
manoj-kumar1 <manojksh5@gmail.com>
2014-02-02 16:35:00 +0530

    First cut: Problem statement

---
+++ UnityDemo-Problem/UnityDemo.Security.Data/UserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityDemo.Security.Data
+{
+    public class UserData : IUserData
+    {
+        public User GetUser(int id)
+        {
+            //Just instantiate and pass..Ideally can come from a database
+            return new User
+                {
+                    Id = id,
+                    Name = "Manoj Kumar",
+                    Age = 28
+                };
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return null;
+        }
+    }
+}
---
+++ UnityDemo-Problem/UnityDemo.Security/UserDomain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityDemo.Common;
+using UnityDemo.Common.Entities.Security;
+using UnityDemo.Security.Data;
+using UnityDemo.Security.Mappers;
+
+namespace UnityDemo.Security
+{
+    using Data = UnityDemo.Security.Data;
+
+    public class UserDomain : IUserDomain
+    {
+        public readonly IUserData _data;
+
+        public UserDomain(IUserData data)
+        {
+            _data = data;
+        }
+
+        public IUser GetUser(int userId)
+        {
+            var user = _data.GetUser(userId);
+
+            return user.MapTo();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return null;
+        }
+    }
+}
